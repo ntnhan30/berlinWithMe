5d5648965db9d304594c377e5477200bcff38f3f
trong nhan nguyen <ntnhan30@gmail.com>
2018-09-03 09:13:14 +0200

    show events on the home page

--- starterCode/routes/index.js
+++ starterCode/routes/index.js
@@ -1,9 +1,28 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Event = require("../models/Event")
+const User = require("../models/Event")
+
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  Event.find()
+    .populate('_owner')
+    .then(events => {
+      res.render('index', {
+        events
+      });
+    })
 });
+
+router.get('/:owner/events/:id', (req, res, next) => {
+  Event.findById(req.params.id)
+    .then(event => {
+      res.render('event/event-details', {
+        event
+      });
+    })
+});
+
 
 module.exports = router;
