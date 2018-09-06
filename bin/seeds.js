0bf7339b396e442f7d2727568eb64806ceadc1f5
trong nhan nguyen <ntnhan30@gmail.com>
2018-09-06 11:11:03 +0200

    Change DataBase back to online

--- bin/seeds.js
+++ bin/seeds.js
@@ -5,8 +5,8 @@ const User=require("../models/User")
 const mongoose = require("mongoose");
 
 mongoose
-  .connect('mongodb://localhost/berlin')
-  // .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
+  // .connect('mongodb://localhost/berlin')
+  .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
   .then(() => {
     console.log('Connected to Mongo!')
   }).catch(err => {
